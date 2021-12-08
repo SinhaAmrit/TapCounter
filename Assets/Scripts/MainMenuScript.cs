17aab009d23886cc46790f50bbd00f003c039b03
SinhaAmrit <purpleplots@gmail.com>
2021-12-08 22:41:12 +0530

    Button Detection Added

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    public GameObject MainMenu;
+    public GameObject Settings;
+    public GameObject HowToPlay;
+    public GameObject Credits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenu.SetActive(true);
+        Settings.SetActive(false);
+        HowToPlay.SetActive(false);
+        Credits.SetActive(false);
+    }
+    public void SettingsBtnClicked()
+    {
+        Settings.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void HowToPlayBtnClicked()
+    {
+        HowToPlay.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void CreditsBtnClicked()
+    {
+        Credits.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+    public void BackBtnClicked() 
+    {
+        MainMenu.SetActive(true);
+        Settings.SetActive(false);
+        HowToPlay.SetActive(false);
+        Credits.SetActive(false);
+    }
+}
